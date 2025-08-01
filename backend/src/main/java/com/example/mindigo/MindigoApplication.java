d7d6baa5bf692e28e908d324a9e101a0b6f49699
SoumikBhattacharjee31 <soumikbhat330@gmail.com>
2025-08-02 00:09:41 +0600

    init

---
+++ backend/src/main/java/com/example/mindigo/MindigoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mindigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindigoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/mindigo/MindigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mindigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
