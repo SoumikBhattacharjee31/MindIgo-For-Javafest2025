f8ab22a016435c11718a1037ee0cc6b0d0de22b7
Somik <somikdasguptabd@gmail.com>
2025-08-06 14:03:37 +0600

    allows /api/v1/auth/** and /api/v1/**/test w/o auth

--- backend/services/gateway-server/src/main/java/com/mindigo/gateway_server/config/RouteValidator.java
+++ backend/services/gateway-server/src/main/java/com/mindigo/gateway_server/config/RouteValidator.java
@@ -3,23 +3,22 @@ package com.mindigo.gateway_server.config;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.function.Predicate;
 
 @Component
 public class RouteValidator {
 
-    public static final List<String> openApiEndpoints = List.of(
-            "/eureka",
-            "/api/v1/auth/register",
-            "/api/v1/auth/validate",
-            "/api/v1/auth/login",
-            "/api/v1/auth/get-otp"
-//            "/api/v1/auth/loginOtp"
-    );
+    public Predicate<ServerHttpRequest> isSecured = request -> {
+        String path = request.getURI().getPath();
 
-    public Predicate<ServerHttpRequest> isSecured =
-            request -> openApiEndpoints
-                    .stream()
-                    .noneMatch(uri -> request.getURI().getPath().startsWith(uri));
+        // Allow all /api/v1/auth/** endpoints
+        if (path.startsWith("/api/v1/auth/")) {
+            return false;
+        }
+
+        // Allow any /api/v1/**/test
+        return !path.matches("^/api/v1/.*/test/?$");
+
+        // Secure everything else
+    };
 }
