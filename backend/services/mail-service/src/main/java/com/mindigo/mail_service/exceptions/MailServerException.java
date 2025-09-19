fc853e8ddd21bdf673b5f3570c164afd6e30b4fc
SoumikBhattacharjee31 <soumikbhat330@gmail.com>
2025-09-19 19:48:50 +0600

    removed admin and mail service backend and started redis

--- backend/services/mail-service/src/main/java/com/mindigo/mail_service/exceptions/MailServerException.java
+++
@@ -1,7 +0,0 @@
-package com.mindigo.mail_service.exceptions;
-
-public class MailServerException extends RuntimeException {
-    public MailServerException(String message) {
-        super(message);
-    }
-}
