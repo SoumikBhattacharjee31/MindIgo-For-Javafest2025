c2329c4530f79ecabaff3af8734f399b6eb3546f
SoumikBhattacharjee31 <soumikbhat330@gmail.com>
2025-09-19 12:09:50 +0600

    refactoring

--- backend/services/content-service/src/main/java/com/mindigo/content_service/controllers/ScateboarderScoreController.java
+++ backend/services/content-service/src/main/java/com/mindigo/content_service/controllers/ScateboarderScoreController.java
@@ -20,7 +20,7 @@ import java.util.Optional;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/v1/content/snowboarder")
+@RequestMapping("/api/v1/game/snowboarder")
 @RequiredArgsConstructor
 public class ScateboarderScoreController {
     private static final Logger logger = LoggerFactory.getLogger(ScateboarderScoreController.class);
--- frontend/app/dashboard/games/infinity-loop/components/BackgroundVideo.tsx
+++ frontend/app/dashboard/games/infinity-loop/components/BackgroundVideo.tsx
@@ -5,7 +5,7 @@ const BackgroundVideo: React.FC = () => {
   return (
     <div className="absolute inset-0 overflow-hidden">
       <video autoPlay loop muted className="min-w-full min-h-full object-cover">
-        <source src="../background_infinity_loop.mov" type="video/mp4" />
+        <source src="/background_infinity_loop.mov" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
